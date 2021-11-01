fc410db0d7ecbccc21721bcf69f3ce6d9221459f
Katelyn <katelynjewel@gmail.com>
2021-11-01 16:56:57 -0700

    commit tree ba68782448b801643559ea66cfab552f6060fb5d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    case array.length 
+        when 1
+            array.join
+        when 2
+            array.join(" and ")
+        when 3
+            array.join(" , ")
+        else
+            array.split(" , ")
+    end  
 end
